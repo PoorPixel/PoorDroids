6f76fe8935a42eff648f5d24889a50acfe05089e
vanitasvitae <heinrich-hux@gmail.com>
2015-06-02 23:36:38 +0200

    Initial commit. Letz do babysteps

---
+++ poorpixel/Main.java
@@ -0,0 +1,15 @@
+package poorpixel;
+
+/**
+ * There goes the bobbycar
+ * *brumm*
+ * @author vanitas
+ *
+ */
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("One day I'll be a beautiful butterfly!");
+	}
+}
